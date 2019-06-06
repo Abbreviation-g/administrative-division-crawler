5cfa4c56c6d270b3faf7abd3f90a337c9fb0a9e7
guoenjing <guoenjing@joshvm.com>
2019-06-06 18:15:06 +0800

    更新Spring Boot版本，更新mysql driver，设置utf8编码

--- administrative-division-crawler/src/main/java/com/my/crawler/SpringWebConfig.java
+++ administrative-division-crawler/src/main/java/com/my/crawler/SpringWebConfig.java
@@ -1,7 +1,14 @@
 package com.my.crawler;
 
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -13,5 +20,23 @@ public class SpringWebConfig extends WebMvcConfigurationSupport {
         AntPathMatcher pathMatcher = new AntPathMatcher();
         pathMatcher.setCaseSensitive(false);
         configurer.setPathMatcher(pathMatcher);
+    }
+    
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(responseBodyConverter());
+        // 这里必须加上加载默认转换器，不然bug玩死人，并且该bug目前在网络上似乎没有解决方案
+        // 百度，谷歌，各大论坛等。你可以试试去掉。
+        addDefaultHttpMessageConverters(converters);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
     }
 }
